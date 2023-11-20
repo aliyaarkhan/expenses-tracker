2ef798fb63dbcfcdff2551841fc9995b1048b1d0
aliyaarkhan <aliyaarkhan007@gmail.com>
2023-11-20 21:31:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+
+import AppStateProvider from "./context/AppState";
+
+function App() {
+  return (
+    <AppStateProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </AppStateProvider>
+  );
+}
+
+export default App;
